d539aa7107de895385ee26602b0cb179a8a34f9d
1303575952 <1303575952@qq.com>
2018-11-07 16:25:41 +0800

    init

---
+++ src/main/java/com/sxu/movieelasticsearch/MovieElasticsearchApplication.java
@@ -0,0 +1,12 @@
+package com.sxu.movieelasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieElasticsearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieElasticsearchApplication.class, args);
+    }
+}
---
+++ src/test/java/com/sxu/movieelasticsearch/MovieElasticsearchApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sxu.movieelasticsearch;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MovieElasticsearchApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
