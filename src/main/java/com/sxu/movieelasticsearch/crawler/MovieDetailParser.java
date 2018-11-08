c1e9c2b9263003f123c75bce28e004d89054984f
1303575952 <1303575952@qq.com>
2018-11-08 09:08:11 +0800

    crawl

---
+++ src/main/java/com/sxu/movieelasticsearch/crawler/MovieDetailParser.java
@@ -0,0 +1,117 @@
+package com.sxu.movieelasticsearch.crawler;
+
+import com.sxu.movieelasticsearch.entity.Movie;
+import lombok.extern.slf4j.Slf4j;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.MessageFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+import static com.sxu.movieelasticsearch.crawler.MovieListParser.userAgentList;
+
+@Component
+@Slf4j
+public class MovieDetailParser {
+
+    public static final String URL_PATTERN = "https://www.dy2018.com/i/{0}.html";
+
+    public Movie parse(String id) throws IOException {
+        String userAgent = userAgentList[new Random().nextInt(userAgentList.length)];
+        String url = MessageFormat.format(URL_PATTERN, id);
+        Document document = Jsoup.connect(url).userAgent(userAgent).timeout(10000).get();
+        Movie movie = new Movie();
+        movie.setId(id);
+        String title = document.select(".title_all h1").text();
+        log.debug("标题:{}", title);
+        movie.setTitle(title);
+        String score = document.select("strong.rank").text();
+        log.debug("评分:{}", score);
+        try {
+            movie.setScore(Float.parseFloat(score));
+        } catch (NumberFormatException e) {
+            log.info(e.getMessage());
+        }
+        String updateDate = document.select("span.updatetime").text();
+        log.debug("发布日期:{}", updateDate);
+        try {
+            DateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+            updateDate = updateDate.replace("发布时间：", "");
+            movie.setUpdateDate(sdf.parse(updateDate));
+            movie.setUpdateDay(LocalDate.parse(updateDate, dtf));
+        } catch (ParseException e) {
+            log.info("发布日期{}解析出错", updateDate);
+        }
+        Elements zoom = document.select("#Zoom");
+        try {
+            String coverUrl = zoom.select("p, div").get(0).select("img").attr("src");
+            log.debug("封面图片:{}", coverUrl);
+            movie.setCoverUrl(coverUrl);
+        } catch (Exception e) {
+            log.info("封面图片解析出错");
+        }
+        List<String> actor = new ArrayList<>();
+        movie.setActor(actor);
+        zoom.select("p, div").forEach(p -> {
+            String text = p.text();
+            try {
+                if (text.startsWith("◎译　　名")) {
+                    log.debug("译名:{}", text);
+                    movie.setTranslatedName(Arrays.asList(text.substring(6).split("/")));
+                } else if (text.startsWith("◎片　　名")) {
+                    log.debug("片名:{}", text);
+                    movie.setName(text.substring(6));
+                } else if (text.startsWith("◎年　　代")) {
+                    log.debug("年代:{}", text);
+                    movie.setYear(Integer.parseInt(text.substring(6)));
+                } else if (text.startsWith("◎产　　地")) {
+                    log.debug("产地:{}", text);
+                    movie.setOrigin(text.substring(6));
+                } else if (text.startsWith("◎类　　别")) {
+                    log.debug("类别:{}", text);
+                    movie.setCategory(Arrays.asList(text.substring(6).split("/")));
+                } else if (text.startsWith("◎片　　长")) {
+                    log.debug("片长:{}", text);
+                    movie.setDuration(Integer.parseInt(text.substring(6)
+                            .replace("分钟", "").replace("Mins", "").trim()));
+                } else if (text.startsWith("◎导　　演")) {
+                    log.debug("导演:{}", text);
+                    movie.setDirector(text.substring(6));
+                } else if (text.startsWith("◎上映日期")) {
+                    log.debug("上映日期:{}", text);
+                    movie.setReleaseDate(text.substring(6));
+                } else if (text.startsWith("◎主　　演") || text.startsWith("　　　　　")) {
+                    log.debug("主演:{}", text);
+                    actor.add(text.substring(6));
+                } else if (text.startsWith("◎简　　介")) {
+                    String description = p.nextElementSibling().text();
+                    log.debug("简介:{}", description);
+                    movie.setDescription(description.substring(2));
+                }
+            } catch (RuntimeException e) {
+                log.info(e.getMessage());
+            }
+        });
+        List<String> downloadUrl = new ArrayList<>();
+        movie.setDownloadUrl(downloadUrl);
+        zoom.select("table a").forEach(a -> {
+            String text = a.text();
+            log.debug("下载地址:{}", text);
+            downloadUrl.add(text);
+        });
+        return movie;
+    }
+
+}
---
+++ src/main/java/com/sxu/movieelasticsearch/web/SearchController.java
@@ -0,0 +1,68 @@
+package com.sxu.movieelasticsearch.web;
+
+import com.sxu.movieelasticsearch.entity.Movie;
+import com.sxu.movieelasticsearch.entity.Page;
+import com.sxu.movieelasticsearch.entity.QueryDTO;
+import com.sxu.movieelasticsearch.repository.IMovieRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@Slf4j
+public class SearchController {
+
+    @Autowired
+    private IMovieRepository movieRepository;
+
+    @GetMapping("/")
+    public String index(Model model) {
+        QueryDTO queryDTO = QueryDTO.builder().minScore(7.0f).orderBy("updateDate").build();
+        Page<Movie> page = movieRepository.query(queryDTO, 1, 6);
+        List<String> recommendWord = new ArrayList<>();
+        if (page != null) {
+            page.getList().forEach(movie -> {
+                String word = movie.getName();
+                String title = movie.getTitle();
+                if (!StringUtils.isEmpty(title) && title.contains("《") && title.contains("》")) {
+                    word = title.substring(title.indexOf('《') + 1, title.indexOf('》'));
+                }
+                recommendWord.add(word);
+            });
+        }
+        model.addAttribute("recommendWord", recommendWord);
+        return "index";
+    }
+
+    @RequestMapping("/s")
+    public String search(
+            @RequestParam("wd") String queryString,
+            @RequestParam(value = "pn", required = false, defaultValue = "1") int pageNo,
+            Model model
+    ) {
+        log.info("搜索参数wd:{},pn:{}", queryString, pageNo);
+        Page<Movie> page = movieRepository.query(queryString, pageNo, 10);
+        model.addAttribute("page", page);
+        model.addAttribute("wd", queryString);
+        return "search";
+    }
+
+    @GetMapping("/detail/{id}")
+    public String detail(
+            @PathVariable("id") String id,
+            Model model
+    ) {
+        Movie movie = movieRepository.get(id);
+        model.addAttribute("movie", movie);
+        return "detail";
+    }
+}
